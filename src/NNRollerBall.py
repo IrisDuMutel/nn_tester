a5772ef0feba754c59f942819b9f52d0e31d82b7
IrisDuMutel <irisfp96@gmail.com>
2021-06-22 18:15:37 +0200

    changing computer

---
+++ src/NNRollerBall.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import keras
+import numpy as np
+import math
+import rospy
+
+
+class NNClass:
+    def __init__(self):
+        #Initialize the NN 
+
+        print("aa")    
+
+    
